5a3f5774e13d3272262f1d6054318829d0a9b3b7
Mutluhan Boz <31347570+MutluhanB@users.noreply.github.com>
2017-10-25 00:11:30 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+//MUTLUHAN BOZ 170202009 TERMİNAL 23
+int main()
+{
+int x ;
+label: printf("Pozitif bir tek sayi giriniz :") ;
+scanf("%d",&x) ;
+if(x>0 && x%2==1){
+for(int i=1 ;i<=((x+1)*0.5) ;i++){
+   for(int j=1 ;j<=i-1 ;j++){
+   printf("   ") ;
+   }
+   for(int j=0 ; j<=(x-((2*i)-1)) ;j++){
+   printf(" X ") ;
+   }
+printf("\n");
+}
+for(int i=1 ;i<=(x+1)*0.5; i++){
+   for(int j=(x+1)*0.5 ;j>i ;j--){
+   printf("   ") ;
+   }
+   for(int j=1 ;j<=(2*i)-1 ; j++){
+   printf(" X ") ;
+   }
+printf("\n");
+}
+}
+else {
+goto label ;
+}
+}
+
+
