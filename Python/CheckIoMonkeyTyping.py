1a54c45e9238b8e6bee7c74159f80ee9597e4257
Mutluhan Boz <31347570+MutluhanB@users.noreply.github.com>
2017-11-17 18:36:15 +0300

    Add files via upload

---
+++ Python/CheckIoMonkeyTyping.py
@@ -0,0 +1,25 @@
+#%%
+
+""" 
+You are given some text potentially including sensible words. 
+You should count how many words are included in the given text.
+ A word should be whole and may be a part of other word. 
+ Text letter case does not matter.
+ Words are given in lowercase and don't repeat. 
+ If a word appears several times in the text, it should be counted only once.
+For example, text - "How aresjfhdskfhskd you?", words - ("how", "are", "you", "hello"). 
+The result will be 3.
+"""
+def count_words(a,b) :
+    count= 0 
+    i = 0
+    bl=list(b)
+    a2=a.lower()
+    while i < len(bl) :
+        if bl[i] in a2 :
+            count += 1 
+        i += 1
+    return count
+    
+
+
