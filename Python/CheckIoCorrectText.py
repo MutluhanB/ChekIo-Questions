2ad8caafec4ae1f7c33aa2f4f8883394fd839f12
Mutluhan Boz <31347570+MutluhanB@users.noreply.github.com>
2017-11-18 16:57:04 +0300

    Update CheckIoCorrectText.py

--- Python/CheckIoCorrectText.py
+++ Python/CheckIoCorrectText.py
@@ -1,4 +1,4 @@
-## Capitalize first letter of a given text and adds dot to the end. 18.11.17
+## Capitalizes first letter of a given text and adds dot to the end. 18.11.17
 import string
 def correct_sentence(a):
     if a[-1] != "." :
