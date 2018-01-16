c18cd6114fcbec7e2ba1f525c698056b9518c68a
Mutluhan Boz <31347570+MutluhanB@users.noreply.github.com>
2018-01-16 22:20:33 +0300

    Add files via upload

---
+++ Primes.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Primes {
+	static void sieveOfEratosthenes(int n) {
+		boolean prime[] = new boolean [n+1];		
+		for(int i=0; i<n; i++) {
+			prime[i] = true ;
+		}
+		for(int i=2; i*i<=n; i++) {
+			if(prime[i] == true) {
+				for(int j=i*2; j<=n; j+=i) {
+					prime[j] = false;
+				}
+			}
+		}
+		for(int i=2; i<n; i++) {
+			if(prime[i] == true) {
+			System.out.printf("\n%d",i);
+			}
+		}	
+	}
+	
+	public static void main(String[] args) {
+		Scanner read = new Scanner(System.in);
+		System.out.print("Sayi Giriniz : ");
+		int n = read.nextInt();
+		sieveOfEratosthenes(n);
+	
+	}
+
+}
